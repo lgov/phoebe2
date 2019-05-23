4e736cf01cc4b05aa165e69a7d5104f3661d588a
Kyle Conroy <kyleconroy@gmail.com>
2019-05-23 12:15:18 -0400

    test_constructors: include default_triple and iterate over force_build
    still need to do the following:
    * remove if force_build statement once cached default_triples are supported
    * add default_quadruple and default_doubledouble once implemented and tested

--- tests/nosetests/test_constructors/test_constructors.py
+++ tests/nosetests/test_constructors/test_constructors.py
@@ -6,14 +6,22 @@ import phoebe
 def test_all():
     print("phoebe.Bundle() ...")
     b = phoebe.Bundle()
-    print("phoebe.default_star()) ...")
-    b = phoebe.default_star()
-    print("phoebe.default_binary() ...")
-    b = phoebe.default_binary()
-    print("phoebe.default_binary(contact_envelope=True) ...")
-    b = phoebe.default_binary(contact_envelope=True)
-    # b = phoebe.default_triple(inner_as_primary=True)
-    # b = phoebe.default_triple(inner_as_primary=False)
+
+    for force_build in [True, False]:
+        print("phoebe.default_star(force_build={}) ...".format(force_build))
+        b = phoebe.default_star(force_build=force_build)
+        print("phoebe.default_binary(force_build={}) ...".format(force_build))
+        b = phoebe.default_binary(force_build=force_build)
+        print("phoebe.default_binary(contact_envelope=True, force_build={}) ...".format(force_build))
+        b = phoebe.default_binary(contact_envelope=True, force_build=force_build)
+        if force_build:
+            print("phoebe.default_triple(hierarchy='21', force_build={}) ...".format(force_build))
+            b = phoebe.default_triple(hierarchy='21', force_build=force_build)
+            print("phoebe.default_triple(hierarchy='12', force_build={}) ...".format(force_build))
+            b = phoebe.default_triple(hierarchy='12', force_build=force_build)
+            print("phoebe.default_triple(hierarchy='12', contact_envelope=True, force_build={}) ...".format(force_build))
+            b = phoebe.default_triple(hierarchy='12', contact_envelope=True, force_build=force_build)
+
 
 if __name__ == '__main__':
     logger = phoebe.logger(clevel='WARNING')
